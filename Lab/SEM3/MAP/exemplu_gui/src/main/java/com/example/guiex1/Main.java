1c1167be07e89063b2fe88f80d2d394f1483202c
Petru Zdroba <petru.zdroba@gmail.com>
2025-11-24 21:12:38 +0200

    y2s1w9: map gui example

---
+++ Lab/SEM3/MAP/exemplu_gui/src/main/java/com/example/guiex1/Main.java
@@ -0,0 +1,45 @@
+package com.example.guiex1;
+
+import com.example.guiex1.config.Config;
+import com.example.guiex1.domain.Utilizator;
+import com.example.guiex1.domain.UtilizatorValidator;
+import com.example.guiex1.repository.Repository;
+import com.example.guiex1.repository.dbrepo.UtilizatorDbRepository;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ok");
+       //final String fileName=ApplicationContext.getPROPERTIES().getProperty("data.socialnetwork.users");
+//        String fileName="data/users.csv";
+//        Repository0<Long,Utilizator> userFileRepository = new UtilizatorFile0(fileName
+//                , new UtilizatorValidator());
+//
+//        System.out.println("Reading data from file");
+//        Repository<Long,Utilizator> userFileRepository2 = new UtilizatorFile(fileName
+//                , new UtilizatorValidator());
+//        userFileRepository2.findAll().forEach(x-> System.out.println(x));
+////
+//        System.out.println("Reading data from database");
+//        final String url = ApplicationContext.getPROPERTIES().getProperty("database.socialnetwork.url");
+//        final String username= ApplicationContext.getPROPERTIES().getProperty("databse.socialnetwork.username");
+//        final String pasword= ApplicationContext.getPROPERTIES().getProperty("database.socialnetwork.pasword");
+//        System.out.println("Reading data from file");
+
+        String url = Config.getProperties().getProperty("db.url");
+        String username = Config.getProperties().getProperty("db.username");
+        String pasword = Config.getProperties().getProperty("db.password");
+//        String username="postgres";
+//        String pasword="postgres";
+//        String url="jdbc:postgresql://localhost:5434/socialnetwork";
+        Repository<Long, Utilizator> userFileRepository3 =
+                new UtilizatorDbRepository(url,username, pasword,  new UtilizatorValidator());
+
+        userFileRepository3.findAll().forEach(x-> System.out.println(x));
+
+        HelloApplication.main(args);
+
+    }
+}
+
+
---
+++ Lab/SEM3/MAP/exemplu_gui/src/main/java/com/example/guiex1/controller/UtilizatorController.java
@@ -0,0 +1,69 @@
+package com.example.guiex1.controller;
+
+import com.example.guiex1.domain.Utilizator;
+import com.example.guiex1.domain.ValidationException;
+import com.example.guiex1.services.UtilizatorService;
+import com.example.guiex1.utils.events.UtilizatorEntityChangeEvent;
+import com.example.guiex1.utils.observer.Observer;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class UtilizatorController implements Observer<UtilizatorEntityChangeEvent> {
+    UtilizatorService service;
+    ObservableList<Utilizator> model = FXCollections.observableArrayList();
+
+
+    @FXML
+    TableView<Utilizator> tableView;
+    @FXML
+    TableColumn<Utilizator,String> tableColumnFirstName;
+    @FXML
+    TableColumn<Utilizator,String> tableColumnLastName;
+
+
+    public void setUtilizatorService(UtilizatorService service) {
+        this.service = service;
+        initModel();
+    }
+
+    @FXML
+    public void initialize() {
+        //TODO
+    }
+
+    private void initModel() {
+        //TODO
+    }
+
+    @Override
+    public void update(UtilizatorEntityChangeEvent utilizatorEntityChangeEvent) {
+
+    }
+
+
+
+    public void handleDeleteUtilizator(ActionEvent actionEvent) {
+        Utilizator user=(Utilizator) tableView.getSelectionModel().getSelectedItem();
+        if (user!=null) {
+            Utilizator deleted= service.deleteUtilizator(user.getId());
+        }
+    }
+
+
+
+
+
+
+
+}
