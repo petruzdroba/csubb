bdfa0ceda87d73ced44b007fa33728f097b61e93
Petru Zdroba <petru.zdroba@gmail.com>
2025-12-09 16:10:24 +0200

    added basic log in feature and log out features

--- social-network-petruzdroba/src/main/java/com/ui/controllers/AuthViewController.java
+++ social-network-petruzdroba/src/main/java/com/ui/controllers/AuthViewController.java
@@ -1,12 +1,20 @@
 package com.ui.controllers;
 
+import com.exceptions.RepositoryException;
 import com.service.UserService;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.awt.*;
+import java.io.IOException;
+import java.sql.SQLException;
 
 public class AuthViewController {
     private UserService service;
@@ -20,4 +28,52 @@ public class AuthViewController {
     @FXML private TextField emailField;
     @FXML private PasswordField passwordField;
     @FXML private Button loginButton;
+
+    @FXML
+    private void logIn(){
+        try{
+            String email = emailField.getText();
+            String password = passwordField.getText();
+
+            boolean success = service.logIn(email, password);
+
+            if(success) {
+                openMessageView();
+            } else {
+                showError("Invalid credentials");
+                passwordField.clear();
+            }
+        } catch (SQLException | RepositoryException e) {
+            showError(e.getMessage());
+        }
+    }
+
+    private void openMessageView(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/message-view.fxml"));
+            Parent root = loader.load();
+            String darkTheme = getClass().getResource("/dark-theme.css").toExternalForm();
+            MessageViewController controller = loader.getController();
+
+            Stage stage = (Stage) loginButton.getScene().getWindow();
+
+            Scene scene = new Scene(root);
+            controller.setUserService(service);
+            scene.getStylesheets().add(darkTheme);
+            stage.setScene(scene);
+            stage.setTitle("Message Screen");
+            stage.show();
+
+        } catch (IOException e) {
+            showError("Cannot load message screen: " + e.getMessage());
+        }
+    }
+
+    protected void showError(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
