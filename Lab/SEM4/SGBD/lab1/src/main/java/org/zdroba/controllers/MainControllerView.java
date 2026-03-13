379f26f768586e57fb58988b08a1e80fff6bb161
Petru Zdroba <petru.zdroba@gmail.com>
2026-03-13 14:42:35 +0200

    y2s2w3: base adding editing and deleting

--- Lab/SEM4/SGBD/lab1/src/main/java/org/zdroba/controllers/MainControllerView.java
+++ Lab/SEM4/SGBD/lab1/src/main/java/org/zdroba/controllers/MainControllerView.java
@@ -3,9 +3,9 @@ package org.zdroba.controllers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
 import org.zdroba.entity.Park;
 import org.zdroba.entity.Tag;
 import org.zdroba.entity.Trail;
@@ -20,6 +20,8 @@ public class MainControllerView {
     @FXML private TableView<Park> parksTable;
     @FXML private TableColumn<Park, Long> parkIdCol;
     @FXML private TableColumn<Park, String> parkNameCol;
+    @FXML private TableColumn<Park, String> parkCountryCol;
+
 
     @FXML private TableView<Trail> trailsTable;
     @FXML private TableColumn<Trail, Long> trailIdCol;
@@ -34,10 +36,16 @@ public class MainControllerView {
     private final ObservableList<Trail> trailList = FXCollections.observableArrayList();
     private final ObservableList<Tag> tagList = FXCollections.observableArrayList();
 
+    @FXML private TextField trailNameField;
+    @FXML private TextField trailLengthField;
+
+    @FXML private TextField tagNameField;
+
     @FXML
     public void initialize() {
         parkIdCol.setCellValueFactory(new PropertyValueFactory<>("id"));
         parkNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        parkCountryCol.setCellValueFactory(new PropertyValueFactory<>("county"));
 
         trailIdCol.setCellValueFactory(new PropertyValueFactory<>("id"));
         trailNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
@@ -78,9 +86,97 @@ public class MainControllerView {
         );
     }
 
-    @FXML private void onAddPark() {}
-    @FXML private void onEditPark() {}
-    @FXML private void onDeletePark() {}
+    @FXML private void onAddPark() {
+        Dialog<Park> dialog = new Dialog<>();
+        dialog.setTitle("Add Park");
+
+        GridPane grid = new GridPane();
+
+        TextField nameField = new TextField();
+        TextField countryField = new TextField();
+
+        grid.add(new Label("Name:"), 0, 0);
+        grid.add(nameField, 1, 0);
+        grid.add(new Label("Country:"), 0, 1);
+        grid.add(countryField, 1, 1);
+
+        dialog.getDialogPane().setContent(grid);
+        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+
+        dialog.setResultConverter(btn -> {
+            if (btn == ButtonType.OK) {
+                String name = nameField.getText().trim();
+                String country = countryField.getText().trim();
+                if (name.isEmpty() || country.isEmpty()) {
+                    showError("Park attributes cannot be empty");
+                    return null;
+                }
+                return new Park(name, country);
+            }
+            return null;
+        });
+
+        dialog.showAndWait().ifPresent(park -> {
+            parkRepository.add(park);
+            loadParks();
+        });
+    }
+
+    @FXML private void onEditPark() {
+        Dialog<Park> dialog = new Dialog<>();
+        dialog.setTitle("Edit Park");
+
+        GridPane grid = new GridPane();
+
+        Park selected = parksTable.getSelectionModel().getSelectedItem();
+        TextField nameField = new TextField(selected.getName());
+        TextField countryField = new TextField(selected.getCounty());
+
+        grid.add(new Label("Name:"), 0, 0);
+        grid.add(nameField, 1, 0);
+        grid.add(new Label("Country:"), 0, 1);
+        grid.add(countryField, 1, 1);
+
+        dialog.getDialogPane().setContent(grid);
+        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+
+        dialog.setResultConverter(btn -> {
+            if (btn == ButtonType.OK) {
+                String name = nameField.getText().trim();
+                String country = countryField.getText().trim();
+                if (name.isEmpty() || country.isEmpty()) {
+                    showError("Park attributes cannot be empty");
+                    return null;
+                }
+                return new Park(name, country);
+            }
+            return null;
+        });
+
+        dialog.showAndWait().ifPresent(park -> {
+            parkRepository.update(selected.getId(), park);
+            loadParks();
+        });
+    }
+
+    @FXML private void onDeletePark() {
+        Park selected = parksTable.getSelectionModel().getSelectedItem();
+        if (selected == null) {
+            showError("No park selected!");
+            return;
+        }
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Delete " + selected.getName());
+        alert.setContentText("Are you sure you want to delete " + selected.getName() + " " + selected.getCounty());
+
+        alert.showAndWait().ifPresent(btn -> {
+            if (btn == ButtonType.OK) {
+                parkRepository.delete(selected.getId());
+                loadParks();
+            }
+        });
+    }
 
     @FXML private void onAddTrail() {}
     @FXML private void onEditTrail() {}
@@ -89,4 +185,12 @@ public class MainControllerView {
     @FXML private void onAddTag() {}
     @FXML private void onEditTag() {}
     @FXML private void onDeleteTag() {}
+
+    private void showError(String message){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
