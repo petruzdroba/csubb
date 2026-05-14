a1491765710429690b9e677bfad24852f27054f4
petruzdroba <petru.zdroba@gmail.com>
2026-05-14 16:12:26 +0300

    y2s2w10: SGBD lab5 init

---
+++ Lab/SEM4/SGBD/lab5/src/main/resources/data.sql
@@ -0,0 +1,26 @@
+INSERT INTO departments (name)
+VALUES ('HR'),
+       ('IT'),
+       ('Sales'),
+       ('Marketing'),
+       ('Accounting'),
+       ('Legal'),
+       ('Operations'),
+       ('Design'),
+       ('Product'),
+       ('Security');
+
+SET SESSION cte_max_recursion_depth = 10001;
+
+INSERT INTO employees (name, email, salary, department_id)
+WITH RECURSIVE seq AS (
+    SELECT 1 AS n
+    UNION ALL
+    SELECT n + 1 FROM seq WHERE n < 5000
+)
+SELECT
+    CONCAT('Employee_', n),
+    CONCAT('employee', n, '@company.com'),
+    ROUND(30000 + (RAND() * 70000), 2),
+    (MOD(n, 10) + 1)
+FROM seq;
